43aa82f6fe4aa5f6c4b1e3477aa6c7f990a07106
simasguga <163364952+simasguga@users.noreply.github.com>
2024-03-13 22:27:33 -0300

    código com a resolução do problema!

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log()
+
+// No presente código a resolução não utiliza um meio/forma de diferença entre tipos de combustível.
+// Abaixo do mesmo terá o código para a resolução com o tipo de gasolina definido.
+
+const valorCombustivelGasolina = 5.58;
+const valorCombustivelEtanol = 3.34;
+const kmPorLitro = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitro;
+
+const valorGastoViagem = litrosConsumidos * valorCombustivelGasolina;
+console.log(`Valor gasto na viagem de ${distanciaEmKm}km é de R$${valorGastoViagem} reais!`)
